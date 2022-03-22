c9276588aad143273352d4f97dc46f9e11c53be9
hugosenari <hugosenari@gmail.com>
2022-03-22 04:31:53 -0300

    chore: re index nhsl

--- master/flake.nix
+++ master/flake.nix
@@ -10,7 +10,7 @@
   inputs.src-nhsl-master.flake = false;
   inputs.src-nhsl-master.ref   = "refs/heads/master";
   inputs.src-nhsl-master.owner = "twist-vector";
-  inputs.src-nhsl-master.repo  = "nhsl.git";
+  inputs.src-nhsl-master.repo  = "nhsl";
   inputs.src-nhsl-master.type  = "github";
   
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
